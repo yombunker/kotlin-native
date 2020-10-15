5e429f0397432d7efe58e61b97704337ec33560d
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2020-10-15 13:26:57 +0300

    Move legacy memory manager into its own module

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -37,7 +37,9 @@ bitcode {
             "${target}Relaxed",
             "${target}ProfileRuntime",
             "${target}Objc",
-            "${target}ExceptionsSupport"
+            "${target}ExceptionsSupport",
+            "${target}LegacyMemoryManager",
+            "${target}ExperimentalMemoryManager"
         )
         includeRuntime()
         linkerArgs.add(project.file("../common/build/bitcode/main/$target/hash.bc").path)
@@ -92,6 +94,14 @@ bitcode {
         dependsOn("downloadGoogleTest")
         headersDirs += googletest.headersDirs
     }
+
+    create("legacy_memory_manager") {
+        includeRuntime()
+    }
+
+    create("experimental_memory_manager") {
+        includeRuntime()
+    }
 }
 
 targetList.forEach { targetName ->
@@ -100,6 +110,7 @@ targetList.forEach { targetName ->
             "${targetName}StdAllocRuntimeTests",
             listOf(
                 "${targetName}Runtime",
+                "${targetName}LegacyMemoryManager",
                 "${targetName}Strict",
                 "${targetName}Release",
                 "${targetName}StdAlloc"
@@ -113,6 +124,7 @@ targetList.forEach { targetName ->
             "${targetName}MimallocRuntimeTests",
             listOf(
                 "${targetName}Runtime",
+                "${targetName}LegacyMemoryManager",
                 "${targetName}Strict",
                 "${targetName}Release",
                 "${targetName}Mimalloc",
@@ -122,9 +134,22 @@ targetList.forEach { targetName ->
         includeRuntime()
     }
 
+    createTestTask(
+            project,
+            "${targetName}ExperimentalMMRuntimeTests",
+            listOf(
+                "${targetName}Runtime",
+                "${targetName}ExperimentalMemoryManager",
+                "${targetName}Release",
+                "${targetName}Mimalloc",
+                "${targetName}OptAlloc"
+            )
+    )
+
     tasks.register("${targetName}RuntimeTests") {
         dependsOn("${targetName}StdAllocRuntimeTests")
         dependsOn("${targetName}MimallocRuntimeTests")
+        dependsOn("${targetName}ExperimentalMMRuntimeTests")
     }
 }
 
@@ -142,6 +167,10 @@ val hostStdAllocRuntimeTests by tasks.registering {
 
 val hostMimallocRuntimeTests by tasks.registering {
     dependsOn("${hostName}MimallocRuntimeTests")
+}
+
+val hostExperimentalMMRuntimeTests by tasks.registering {
+    dependsOn("${hostName}ExperimentalMMRuntimeTests")
 }
 
 val assemble by tasks.registering {
